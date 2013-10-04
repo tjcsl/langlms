3720ffad769efb318289c7003878c33508e71d08
fwilson <fwilson@fwilson.me>
2013-10-04 16:27:29 -0400

    fix syntax

--- sql/assessment.sqlite.sql
+++ sql/assessment.sqlite.sql
@@ -3,13 +3,13 @@ DROP TABLE IF EXISTS assessment_items;
 DROP TABLE IF EXISTS assessment_responses;
 
 CREATE TABLE assessment (
-    aid INTEGER AUTOINCREMENT PRIMARY KEY,
+    aid INTEGER PRIMARY KEY AUTOINCREMENT,
     cid INTEGER NOT NULL,
     name STRING DEFAULT "Assessment"
 );
 
 CREATE TABLE assessment_items (
-    itemid INTEGER AUTOINCREMENT PRIMARY KEY,
+    itemid INTEGER PRIMARY KEY AUTOINCREMENT,
     aid INTEGER NOT NULL,
     qtype INTEGER DEFAULT 0,
     qtitle STRING DEFAULT "Question",
@@ -21,7 +21,7 @@ CREATE TABLE assessment_items (
 );
 
 CREATE TABLE assessments_responses (
-    rid INTEGER AUTOINCREMENT PRIMARY KEY,
+    rid INTEGER PRIMARY KEY AUTOINCREMENT,
     uid INTEGER NOT NULL,
     itemid INTEGER NOT NULL,
     mcanswer INTEGER DEFAULT 0,
